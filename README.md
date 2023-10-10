6cb3e7199377426343b82cede1ed7f44bbca6c58
unknown <caterinailariopaz@alumnos.tuniversformacion.es>
2023-10-10 11:52:21 +0200

    mock-exam

---
+++ README.md
@@ -0,0 +1,4 @@
+# Ejemplo Simulacro Examen
+
+**Caterina Ilario Paz**
+**Web Developer Student**
---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+const calculateButton = document.getElementById("calculate");
+const lengthOutput = document.getElementById("length-output");
+const widthOutput = document.getElementById("width-output");
+
+calculateButton.addEventListener("click", () => {
+
+    let length = parseFloat(document.getElementById("length-input").value);
+    let width = parseFloat(document.getElementById("width-input").value);
+
+    if (!isNaN(length) && !isNaN(width)){
+        const area = length * width;
+        const perimeter = (2 *length) + (2*width);
+
+        lengthOutput.innerText = `Rectangle area: ${area}`;
+        widthOutput.innerText = `Rectangle perimeter: ${perimeter}`;
+    } else{
+        lengthOutput.innerText = "Invalid values";
+        widthOutput.innerText = "Invalid values";
+    }
+});
+
+});
